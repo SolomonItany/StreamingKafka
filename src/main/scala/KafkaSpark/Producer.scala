531862f0c08fdaf4b7de1042a7365ec594061c99
Solomon Itany <itanysolo@gmail.com>
2024-03-04 20:54:18 -0500

    changes

--- src/main/scala/KafkaSpark/Producer.scala
+++ src/main/scala/KafkaSpark/Producer.scala
@@ -1,28 +1,23 @@
 package KafkaSpark
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-//import spark.implicits._
-//import scala.io.Source
-import requests._
 
-//import requests._
+import org.apache.spark.sql.{SparkSession, Encoders}
+import scala.io.Source
+
 object Producer {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Producer")
-      .master("local[*]").getOrCreate()
-    while (true){
-        import spark.implicits._
-        import requests._
-        val apiUrl = "https://api.tfl.gov.uk/Line/victoria/Arrivals?app_id=92293faa428041caad3dd647d39753a0&app_key=ba72936a3db54b4ba5792dc8f7acc043"
-        val response = get(apiUrl, headers = headers)
-        val total = response.text()
-        val dfFromText = spark.read.json(Seq(total).toDS)
-        dfFromText.show(10)
-    }
+      .master("local[*]")
+      .getOrCreate()
 
+    val url = "https://api.tfl.gov.uk/Line/victoria/Arrivals?app_id=92293faa428041caad3dd647d39753a0&app_key=ba72936a3db54b4ba5792dc8f7acc043"
 
+    // Fetch JSON data from the URL
+    val result = Source.fromURL(url).mkString
 
+    // Create a DataFrame from the JSON data
+    val jsonData = spark.read.json(spark.createDataset(Seq(result))(Encoders.STRING))
+    jsonData.show()
   }
 }
 
