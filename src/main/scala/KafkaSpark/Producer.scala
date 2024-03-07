a671607ca8db5997f3db1fcf702b746704c40a2b
Solomon Itany <itanysolo@gmail.com>
2024-03-07 09:25:26 -0500

    changes

--- src/main/scala/KafkaSpark/Producer.scala
+++ src/main/scala/KafkaSpark/Producer.scala
@@ -6,26 +6,27 @@ import scala.io.Source
 object Producer {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("Producer").master("local[*]").getOrCreate()
-    val url = "http://18.133.73.36:5000/Insurance_Claims"
-    // Fetch JSON data from the URL
-    val result = Source.fromURL(url).mkString
+    while (true) {
+      val url = "http://18.133.73.36:5000/Insurance_Claims"
+      // Fetch JSON data from the URL
+      val result = Source.fromURL(url).mkString
 
-    // Create a DataFrame from the JSON data
-    val jsonData = spark.read.json(spark.createDataset(Seq(result))(Encoders.STRING))
+      // Create a DataFrame from the JSON data
+      val jsonData = spark.read.json(spark.createDataset(Seq(result))(Encoders.STRING))
 
-    val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
-    val topicName: String = "InsuranceClaims"
+      val kafkaServer: String = "ip-172-31-3-80.eu-west-2.compute.internal:9092"
+      val topicName: String = "InsuranceClaims2"
 
-    jsonData.selectExpr("to_json(struct(*)) AS value")
-      .selectExpr("CAST(value AS STRING)")
-      .write
-      .format("kafka")
-      .option("kafka.bootstrap.servers", kafkaServer)
-      .option("topic", topicName).save()
+      jsonData.selectExpr("to_json(struct(*)) AS value")
+        .selectExpr("CAST(value AS STRING)")
+        .write
+        .format("kafka")
+        .option("kafka.bootstrap.servers", kafkaServer)
+        .option("topic", topicName).save()
 
-    println("message is loaded to kafka topic")
-    Thread.sleep(10000) // wait for 10 seconds before making the next call
-
+      println("message is loaded to kafka topic")
+      Thread.sleep(10000) // wait for 10 seconds before making the next call
+    }
   }
 }
 
